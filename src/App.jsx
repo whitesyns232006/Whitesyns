a57b5b84b178bfdd8db64ba299deee8e85c61f4d
whitesyns232006 <whitesyns232006@gmail.com>
2026-07-01 21:16:27 +0500

    Updated button text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './Pages/HomePage';
@@ -33,52 +34,81 @@ const RouteWrapper = ({ children }) => {
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <ScrollToTop />
-      <Header />
-      
-      <main className="grow">
-        <Routes>
-          <Route path="/" element={
-            <RouteWrapper>
-              <Home />
-            </RouteWrapper>
-          } />
-          <Route path="/about" element={
-            <RouteWrapper>
-              <AboutUs />
-            </RouteWrapper>
-          } />
-          <Route path="/faq" element={
-            <RouteWrapper>
-              <FAQs />
-            </RouteWrapper>
-          } />
-          <Route path="/terms" element={
-            <RouteWrapper>
-              <TermsOfService />
-            </RouteWrapper>
-          } />
-          <Route path="/orders" element={
-            <RouteWrapper>
-              <OrderForm />
-            </RouteWrapper>
-          } />
-          <Route path="/confirmation" element={
-            <RouteWrapper>
-              <OrderConfirmation />
-            </RouteWrapper>
-          } />
-          <Route path="/track" element={
-            <RouteWrapper>
-              <Home />
-            </RouteWrapper>
-          } />
-        </Routes>
-      </main>
-      
-      <Footer />
-    </div>
+    <HelmetProvider>
+      <div className="flex flex-col min-h-screen">
+        {/* Default Helmet for all pages */}
+        <Helmet>
+          <title>Whitesyns Pakistan</title>
+          <meta name="description" content="Whitesyns - Pakistan's first edible teeth-whitening solution" />
+        </Helmet>
+        
+        <ScrollToTop />
+        <Header />
+        
+        <main className="grow">
+          <Routes>
+            <Route path="/" element={
+              <RouteWrapper>
+                <Helmet>
+                  <title>Whitesyns Pakistan</title>
+                </Helmet>
+                <Home />
+              </RouteWrapper>
+            } />
+            <Route path="/about" element={
+              <RouteWrapper>
+                <Helmet>
+                  <title>About Us - Whitesyns Pakistan</title>
+                </Helmet>
+                <AboutUs />
+              </RouteWrapper>
+            } />
+            <Route path="/faq" element={
+              <RouteWrapper>
+                <Helmet>
+                  <title>FAQs - Whitesyns Pakistan</title>
+                </Helmet>
+                <FAQs />
+              </RouteWrapper>
+            } />
+            <Route path="/terms" element={
+              <RouteWrapper>
+                <Helmet>
+                  <title>Terms of Service - Whitesyns Pakistan</title>
+                </Helmet>
+                <TermsOfService />
+              </RouteWrapper>
+            } />
+            <Route path="/orders" element={
+              <RouteWrapper>
+                <Helmet>
+                  <title>Place Order - Whitesyns Pakistan</title>
+                </Helmet>
+                <OrderForm />
+              </RouteWrapper>
+            } />
+            <Route path="/confirmation" element={
+              <RouteWrapper>
+                <Helmet>
+                  <title>Order Confirmation - Whitesyns Pakistan</title>
+                </Helmet>
+                <OrderConfirmation />
+              </RouteWrapper>
+            } />
+            <Route path="/track" element={
+              <RouteWrapper>
+                <Helmet>
+                  <title>Track Shipment - Whitesyns Pakistan</title>
+                </Helmet>
+                <Home />
+              </RouteWrapper>
+            } />
+          </Routes>
+        </main>
+        
+        <Footer />
+      </div>
+    </HelmetProvider>
   );
 }
 
